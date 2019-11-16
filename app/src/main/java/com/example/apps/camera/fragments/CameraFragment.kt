584eb22c885232ee80db08c1d9cfd5fa61cd548d
Hideharu Nagakura <nagakura@fout.jp>
2019-11-16 14:43:33 +0900

    add: Add a Destination to the Navigation Graph

---
+++ app/src/main/java/com/example/apps/camera/fragments/CameraFragment.kt
@@ -0,0 +1,17 @@
+package com.example.apps.camera.fragments
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.apps.camera.R
+
+
+class CameraFragment : Fragment() {
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? =
+        inflater.inflate(R.layout.fragment_camera, container, false)
+
+
+}
